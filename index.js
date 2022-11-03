4e1189d35cefc14f01bd96ca7b3ad1c8a3eb14f6
Blake Arnold <blakearnold@users.noreply.github.com>
2022-11-03 11:14:45 -0700

    set warn/error/notice (#2)
    * set warn/error/notice
    
    * parsefloat

--- index.js
+++ index.js
@@ -41,9 +41,17 @@ try {
         const line = pl.region.startLine;
         const endLine = pl.region.endLine;
         // https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#using-workflow-commands-to-access-toolkit-functions
-
+        const message = buildMessage(result, rule)
         // https://github.com/actions/toolkit/tree/main/packages/core#annotations
-        core.warning(buildMessage(result, rule), {title: rule.shortDescription.text, file: fileName, startLine: line, endLine: endLine});
+        const annotation = {title: rule.shortDescription.text, file: fileName, startLine: line, endLine: endLine}
+        const securitySeverity = parseFloat(rule.properties['security-severity'])
+        if (securitySeverity >= 8) {
+          core.error(message, annotation);
+        } else if (securitySeverity >= 4) {
+          core.warning(message, annotation);
+        } else {
+          core.notice(message, annotation);
+        }
       }
     }
 
